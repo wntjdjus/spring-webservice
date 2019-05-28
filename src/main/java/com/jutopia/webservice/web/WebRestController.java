39c7fbebd5b0ee365431af0c3ede70689382a106
wntjdjus <wntjdjus@naver.com>
2019-05-28 16:38:42 +0900

    [ADD] entity,Controller,DTO,Test

--- src/main/java/com/jutopia/webservice/web/WebRestController.java
+++ src/main/java/com/jutopia/webservice/web/WebRestController.java
@@ -1,13 +1,23 @@
 package com.jutopia.webservice.web;
 
+import com.jutopia.webservice.domain.posts.PostsRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebRestController {
 
+    private PostsRepository postsRepository;
+
     @GetMapping("/hello")
     public String hello(){
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 }
